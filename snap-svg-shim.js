acd7a4a344ba51b184b8db98f048b7cbc88cdb1e
Courtland Allen <courtlandallen@gmail.com>
2015-02-09 22:43:29 -0800

    Expose mina

--- snap-svg-shim.js
+++ snap-svg-shim.js
@@ -1,13 +1,14 @@
 (function () {
-  /* globals define, Snap */
+  /* globals define, mina, Snap */
 
   function generateModule(name, values) {
-    define (name, [], function () {
+    define(name, [], function () {
       'use strict';
 
       return values;
     });
   }
 
+  generateModule('mina', { 'default': mina });
   generateModule('snap-svg', { 'default': Snap });
 })();
